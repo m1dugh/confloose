0597438219213af58d0010ca18a1471817a3207e
romain.le-miere <romain.le-miere@epita.fr>
2022-05-06 18:27:02 +0200

    config: fills up run script

---
+++ run.sh
@@ -0,0 +1,47 @@
+scripts_count=${#scripts[@]}
+
+display_scripts () {
+	echo "Scripts: "
+	for i in ${!scripts[@]}; do
+		echo "  $((i+1))) ${scripts[$i]}"
+	done
+	echo 
+	echo "  0) quit"
+	echo
+}
+
+display_scripts
+
+
+while true; do
+    # Ask for input
+    echo -n "Select: "
+    read selected
+
+    # Check input is numeric
+    re='^[0-9]+$'
+    if ! [[ $selected =~ $re ]] ; then
+        echo "error: Not a number" >&2
+        selected=-1
+        continue
+    fi
+
+    # Check input is in bounds
+    if [ $selected -gt $scripts_count ]; then
+        echo "error: Input value too high"
+        selected=-1
+        continue
+    fi
+
+    # Handle exit option (value 0)
+    if [ $selected -eq 0 ]; then
+        break
+    fi
+
+    echo "-----"
+    index=$((selected - 1))
+    eval "run_${scripts[$index]}"
+
+    echo "-----"
+    display_scripts
+done
